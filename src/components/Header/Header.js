60e1a84be22544893bc6e0a718d77b6b5a98f93f
AlejoMl1 <a.munoz254@uniandes.edu.co>
2022-06-03 12:53:15 -0500

    add sticky dinamic  background to the header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,26 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react'
+import React, {useState} from 'react'
 // import './Header.css' 
 import styles from './Header.module.scss';
 import { Link } from 'react-router-dom';
 
 export default function Header() {
+  const [style, setStyle] = useState({
+    home:true,
+    about:false,
+    skills:false,
+    portfolio:false
+  });
+  const changeStyle = (itemToChange) => {
+    setStyle( {
+        home:false,
+        about:false,
+        skills:false,
+        portfolio:false,
+        [itemToChange]: !style[`${itemToChange}`],
+  });
+  };
+
   return (
     <nav className={styles.main}> 
             {/* <!-- Logo  --> */}
@@ -14,12 +30,23 @@ export default function Header() {
             <ul className={styles.menu}>
                 <li> 
                   <Link to="/" >
-                    <a  className={styles.active}>Home</a>
+                    <a  className={styles[`home-${style.home}`]}
+                        onClick={()=>changeStyle("home")}
+                      >Home
+                    </a>
                   </Link>    
                 </li>
-                <li>  <Link to="/aboutMe" > <a >About</a></Link> </li>
-                <li> <Link to="/skills" ><a>Skills</a> </Link></li>
-                <li> <Link to="/portfolio" ><a>Portfolio</a> </Link>    </li>
+                <li>  <Link to="/aboutMe" > 
+                      <a  className={styles[`about-${style.about}`]}
+                      onClick={()=>changeStyle("about")}>
+                          About
+                      </a>
+                      </Link> 
+                </li>
+                <li> <Link to="/skills" ><a className={styles[`skills-${style.skills}`]}
+                      onClick={()=>changeStyle("skills")}>Skills</a> </Link></li>
+                <li> <Link to="/portfolio" ><a className={styles[`portfolio-${style.portfolio}`]}
+                      onClick={()=>changeStyle("portfolio")}>Portfolio</a> </Link>    </li>
                 {/* <li> <a href="/portfolio">Contact</a>    </li> */}
             </ul>
             <a href="#" className={styles.lang}>En</a>
